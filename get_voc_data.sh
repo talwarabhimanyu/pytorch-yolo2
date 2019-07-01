21f61a4a84177fa6ca15f88df3d19fa6aa8a64d6
Abhimanyu Talwar <p10abhimanyut@iima.ac.in>
2019-07-01 12:39:08 +0800

    minor edit get_voc_data.sh

--- get_voc_data.sh
+++ get_voc_data.sh
@@ -4,9 +4,9 @@
 wget -q https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
 wget -q https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
 wget -q https://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
-tar xf VOCtrainval_11-May-2012.tar
-tar xf VOCtrainval_06-Nov-2007.tar
-tar xf VOCtest_06-Nov-2007.tar
+tar -xf VOCtrainval_11-May-2012.tar
+tar -xf VOCtrainval_06-Nov-2007.tar
+tar -xf VOCtest_06-Nov-2007.tar
 
 python voc_label.py
 cat 2007_train.txt 2007_val.txt 2012_*.txt > voc_train.txt
