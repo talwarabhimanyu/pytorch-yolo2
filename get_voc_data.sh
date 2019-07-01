3169861ce4063ebbf84aba0b30249282b1549c5b
Abhimanyu Talwar <p10abhimanyut@iima.ac.in>
2019-07-01 11:01:51 +0800

    script for prepating VOC data - added source at top

--- get_voc_data.sh
+++ get_voc_data.sh
@@ -1,3 +1,6 @@
+
+# Scrip to download and process Pascal VOC data
+# Source: https://github.com/marvis/pytorch-yolo2
 wget https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
 wget https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
 wget https://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
