f095a2c2a1498e5dd6337f11ace8eb5bb9657e7a
Abhimanyu Talwar <p10abhimanyut@iima.ac.in>
2019-07-01 12:26:35 +0800

    added -q to wget voc_label.py

--- get_voc_data.sh
+++ get_voc_data.sh
@@ -1,9 +1,9 @@
 
 # Scrip to download and process Pascal VOC data
 # Source: https://github.com/marvis/pytorch-yolo2
-wget https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
-wget https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
-wget https://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
+wget -q https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
+wget -q https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
+wget -q https://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
 tar xf VOCtrainval_11-May-2012.tar
 tar xf VOCtrainval_06-Nov-2007.tar
 tar xf VOCtest_06-Nov-2007.tar
